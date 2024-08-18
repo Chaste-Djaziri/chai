02a7d3aad71270907b8cd93499a665c6356f7bd3
djsensei1 <108956116+Djsensei1@users.noreply.github.com>
2024-08-18 22:17:40 +0000

    main commit

--- components/header.tsx
+++ components/header.tsx
@@ -39,7 +39,7 @@ async function UserOrLogin() {
           <UserMenu user={session.user} />
         ) : (
           <Button variant="link" asChild className="-ml-2">
-            <Link href="/login">Login</Link>
+            <Link href="/">Home</Link>
           </Button>
         )}
       </div>
@@ -56,24 +56,19 @@ export function Header() {
         </React.Suspense>
       </div>
       <div className="flex items-center justify-end space-x-2">
-        <a
-          target="_blank"
-          href="https://github.com/vercel/nextjs-ai-chatbot/"
-          rel="noopener noreferrer"
+        <Link
+          href="/login"
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
-          <IconGitHub />
-          <span className="hidden ml-2 md:flex">GitHub</span>
-        </a>
-        <a
-          href="https://vercel.com/templates/Next.js/nextjs-ai-chatbot"
-          target="_blank"
-          className={cn(buttonVariants())}
-        >
-          <IconVercel className="mr-2" />
-          <span className="hidden sm:block">Deploy to Vercel</span>
-          <span className="sm:hidden">Deploy</span>
-        </a>
+          <IconGitHub className="mr-2" />
+          <span className="hidden sm:block">Login</span>
+          <span className="sm:hidden">Login</span>
+        </Link>
+        <Link href="/signup" className={cn(buttonVariants())}>
+          <IconGitHub className="mr-2" />
+          <span className="hidden sm:block">Sign Up</span>
+          <span className="sm:hidden">Sign Up</span>
+        </Link>
       </div>
     </header>
   )
